3368bc7f252bf1a2b2d020d1f8446b42673663bd
Spoctic <66314501+Spoctic@users.noreply.github.com>
2021-01-15 21:16:08 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzYzODQ1Nzg3MjA5MzAyMDcx.X39o0w.0LIKw5zqrauqrwZDnzX_R_KXlWE', /// توكن البوت
+    YT_API_KEY: 'api ', /// مش مهم
+    prefix: '$', // هنا برفكس او مقدمة البوت
+    devs: ['510932001386135562'] //// ايدي حسابك
+}
